a3f5533994c2c8672827f52f4e762777bfbeae37
Jzulu22x <juanzr1015@gmail.com>
2024-11-15 09:00:56 -0500

    feat: creation of the connection to the database in the program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using assessmente_de_empleabilidad.Data;
+
+Env.Load();
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var databaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
+var port = Environment.GetEnvironmentVariable("DB_PORT");
+var username = Environment.GetEnvironmentVariable("DB_USERNAME");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var connectionString = $"server={host};port={port};database={databaseName};uid={username};password={password}";
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.Parse("8.0.20-mysql")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
