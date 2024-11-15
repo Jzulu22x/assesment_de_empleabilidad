58beed29cc00a7f59f2d9a7c9cae95621cdf3971
Jzulu22x <juanzr1015@gmail.com>
2024-11-15 09:01:38 -0500

    feat: commit of ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,11 +9,11 @@ namespace assessmente_de_empleabilidad.Data;
 public class ApplicationDbContext : DbContext
 {
     public DbSet<Admin> Admins {get; set;}
-    public DbSet <Appointment> Appointments {get; set;}
-    public DbSet <Doctor> Doctors {get; set;}    
-    public DbSet <Patient> Patients {get; set;}
     public DbSet <Role> Roles {get; set;}
     public DbSet <Speciality> Specialities {get; set;}
+    public DbSet <Patient> Patients {get; set;}
+    public DbSet <Doctor> Doctors {get; set;}    
+    public DbSet <Appointment> Appointments {get; set;}
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
